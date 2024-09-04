7b2404c3f0e53e7cce00701fecccc1bf78022a83
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-09-04 23:48:31 +0530

    Type Conversion and Type casting

---
+++ C++ Basics/4.type_conversion&casting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+    char alph = 'A';
+    int value = alph;   //Assigned the value of char type alph to inte type value. The "ASCII value" in alph got assigned to int value
+    cout<<value;    //This is called implicit conversion or Type Conversion
+
+    double price = 100.49;
+    int newPrice = (int)price;  //This is called explicit Conversion or Type Casting
+    cout<<(5/(double)2);
+    cout<<newPrice;
+}
