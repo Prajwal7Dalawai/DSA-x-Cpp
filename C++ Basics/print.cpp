5cbac041af4fd632f778c5e8d38c7020dcc6a9a8
Prajwal S Dalawai <109363054+Prajwal7Dalawai@users.noreply.github.com>
2024-09-04 23:49:59 +0530

    Delete C++ Basics/print.cpp

--- C++ Basics/print.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include<conio.h>
-using namespace std;
-main(){ //main function
-    cout<<"Hello World"<<" This is me"<<endl;    //printing on the console with output cascading. cout stands for console out.
-}
