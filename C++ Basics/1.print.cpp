a4937fffc1d2d426b5218eb9253fc45eef4fe9ff
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-09-04 23:45:04 +0530

    Printing on the console with output cascading

---
+++ C++ Basics/1.print.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){ //main function
+    cout<<"Hello World"<<" This is me"<<endl;    //printing on the console with output cascading. cout stands for console out.
+}
