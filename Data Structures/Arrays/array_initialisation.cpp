20ab4e1c6550c63657a2e8dfd733fd07e159eec7
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-09-12 01:02:40 +0530

    Types of initialisation in C++

---
+++ Data Structures/Arrays/array_initialisation.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+main(){
+    int arr[5] = {1,2,3,4,5};   //1st way
+    int arr1[] = {1,2,3,4,5,6,7,8}; //2nd way
+    cout<<sizeof(arr1); //usage of sizeof
+}
