fbd01e08514d3a478e42c2475298f6a1870848fe
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-09-14 13:39:29 +0530

    Vector functions

---
+++ Data Structures/Vectors/vector_functions.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+main(){
+    vector<char> vec = {'p','r','a','j','w','a','l'};
+
+    cout<<vec.size()<<endl;     //size function
+    vec.push_back('s');     //push_back() function
+    vec.pop_back();     //pop_back() function
+    cout<<vec.front()<<endl;    //front function
+    cout<<vec.back()<<endl;     //back function
+    cout<<vec.at(3)<<endl;
+}
