b1dbfb2956bb184ae0bae15db3ceb3ae93d1ee7c
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-09-14 13:39:56 +0530

    Usage of for each loop for vectors

---
+++ Data Structures/Vectors/forEachLoop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+main(){
+    vector<char> vec = {'p','r','a','j','w','a','l'};
+
+    for(char val : vec){
+        cout<<val<<endl;
+    }
+}
