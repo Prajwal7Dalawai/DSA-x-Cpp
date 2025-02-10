592b461ce3409546b9328331f2eb5ebd3155b94d
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2025-02-11 01:08:17 +0530

    Insertion Sort

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void swap(int &a,int &b){
+
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void insertion_sort(vector<int> &arr){
+    int n = arr.size();
+    for(int i = 1; i<n; i++){
+       int key = arr[i];
+       int j = i-1;
+       while(j >=0 && arr[j] > key){
+        arr[j+1] = arr[j];
+        j--;
+       }
+       arr[j+1] = key;
+    }
+}
+
+void display(vector<int> &arr){
+    for(int i=0; i<arr.size(); i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+
+int main(){
+    vector<int> arr;
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    arr.resize(n); // Resize the vector to avoid out-of-bounds access
+    cout<<"Enter the elements of the array: \n";
+    for(int i = 0; i<n; i++)
+    cin>>arr[i];
+    cout<<"Array before sorting: \n";
+    display(arr);
+    insertion_sort(arr);
+    cout<<"Array after sorting: \n";
+    display(arr); // Display the sorted array
+    return 0;
+}
